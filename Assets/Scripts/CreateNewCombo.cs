d84ecbdac0bc9335b3146d80e9f6f550983cf9aa
Danny <gidhub@thoemda.com>
2015-12-19 21:48:26 +0100

    first commit

---
+++ Assets/Scripts/CreateNewCombo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CreateNewCombo : MonoBehaviour {
+
+    public int lengtecombi;
+    public List<Color> kleuren;
+    public GameObject[] doelvakken;
+
+
+    Renderer render;
+    int kleurnr;
+
+
+
+    void Start()
+    {
+
+        nieuwecombinatie();
+    }
+
+    void Update()
+    {
+      
+    }
+
+    void  nieuwecombinatie()
+    {
+        for (int doelen = 0; doelen < doelvakken.Length; doelen++)
+        {
+            render = doelvakken[doelen].gameObject.GetComponent<Renderer>();
+            Color nieuwekleur = kleuren[Random.Range(0, kleuren.Count)];
+            huidigecode.huidigecombinatie.Add(nieuwekleur);
+            render.material.color = nieuwekleur;
+           
+        }
+    }
+
+}
---
+++ Assets/Scripts/huidigecode.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class huidigecode{
+    public static List<Color> huidigecombinatie;
+
+
+
+}
