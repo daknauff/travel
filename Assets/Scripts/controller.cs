bd175f57baa1a5c005a6d48612e425cb255439c6
Danny <gidhub@thoemda.com>
2015-12-20 16:01:20 +0100

    aanmaak controller static class

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -4,39 +4,26 @@ using System.Collections;
 public static class controller
 {
     public static GameObject geselecteerd;
-    public static int level = 4;
-    public static string scene = "Level1";
-    public static int speedincrease = 0;
+    public static Color tempcolor;
+    public static Color selectcolor=Color.red;
 
-    public static void resetlevel()
+    static void selecteer( GameObject GO)
     {
-        Scale = 0.25f;
-        level = 4;
-        scene = "Level1";
-    }
-
-    public static void resetscale()
-    {
-        Scale = 0.25f;
-        level = 4;
-
-    }
-    public static void resetall()
-    {
-        Scale = 0.25f;
-        level = 4;
-        scene = "Level1";
-    }
-    public static void swapscene()
-    {
-        if (scene == "Level1")
+        if (geselecteerd != GO)
         {
-            scene = "Level2";
-        }
-        else
-        {
-            scene = "Level1";
+            if (geselecteerd=null)
+            {
+                geselecteerd = GO;
+                tempcolor = GO.GetComponent<Renderer>().material.color;
+                GO.GetComponent<Renderer>().material.color=selectcolor;
+            }
+            else
+            {
+                geselecteerd.GetComponent<Renderer>().material.color = tempcolor;
+                geselecteerd = GO;
+                tempcolor = GO.GetComponent<Renderer>().material.color;
+                GO.GetComponent<Renderer>().material.color = selectcolor;
+            }
         }
     }
-
 }
